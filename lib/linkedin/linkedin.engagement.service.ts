54d6c4eee65da6e80d92c9d6464700e965c82bb3
thomasuebi <uebsi2804@gmail.com>
2022-10-24 19:37:34 -0700

    texterous integration

--- lib/linkedin/linkedin.engagement.service.ts
+++ lib/linkedin/linkedin.engagement.service.ts
@@ -1,26 +1,31 @@
-import {
-  LinkedinServicesInterface,
-} from "./linkedin.services.interface";
-import { CDPSession, Page } from "puppeteer";
-import { LinkedinAbstractService } from "./linkedin.abstract.service";
-import {gotoUrl} from "../helpers/gotoUrl";
-import {timer} from "../helpers/timer";
-import { shuffle } from "lodash";
+import { LinkedinServicesInterface } from "./linkedin.services.interface"
+import { CDPSession, Page } from "puppeteer"
+import { LinkedinAbstractService } from "./linkedin.abstract.service"
+import { gotoUrl } from "../helpers/gotoUrl"
+import { timer } from "../helpers/timer"
+import { shuffle } from "lodash"
+import axios from "axios"
 
 export class LinkedinEngagementService
   extends LinkedinAbstractService
   implements LinkedinServicesInterface<any>
 {
-  async process(page: Page, cdp: CDPSession, data: any) {
-    gotoUrl(page, "https://www.linkedin.com/feed/");
+  async process(
+    page: Page,
+    cdp: CDPSession,
+    data: {
+      AIGeneratedComments?: { enabled: boolean; texterousApiToken: string }
+    }
+  ) {
+    gotoUrl(page, "https://www.linkedin.com/feed/")
 
-    await this.waitForLoader(page);
+    await this.waitForLoader(page)
 
     await page.mouse.wheel({
       deltaY: 1500,
-    });
+    })
 
-    await timer(4000);
+    await timer(4000)
 
     await page.waitForFunction(() => {
       return (
@@ -33,17 +38,18 @@ export class LinkedinEngagementService
         .map((curr) => {
           return curr
             .closest("button:not(.react-button--active)")
-            ?.getAttribute("id");
+            ?.getAttribute("id")
         })
-        .filter((f) => f);
-    });
+        .filter((f) => f)
+    })
 
     // @ts-ignore
     const ids: Array<{
-      id: string;
-      like: string;
-      comment: string;
-      totalLikes: number;
+      id: string
+      like: string
+      comment: string
+      totalLikes: number
+      text: string
     }> = await page.evaluate(() => {
       return (
         Array.from(
@@ -55,34 +61,40 @@ export class LinkedinEngagementService
         .map((curr) => {
           const parent = curr
             ?.closest("button:not(.react-button--active)")
-            ?.closest(".social-details-social-activity")!;
+            ?.closest(".social-details-social-activity")!
 
+          const content = curr
+            ?.closest(".feed-shared-update-v2")
+            ?.textContent?.replace(/<[^>]*>?/gm, "")
+            ?.replace(/( {2,})/gm, " ")
+            ?.replace(/(\\n){2,}/gm, "\n")
           return {
+            content,
             like: parent
-              .querySelector(
-                '[type="thumbs-up-outline"]'
-              )
+              .querySelector('[type="thumbs-up-outline"]')
               ?.closest("button")
               ?.getAttribute("id"),
             comment: parent
               .querySelector(".comment-button")
               ?.getAttribute("id"),
             id: parent?.getAttribute("id"),
+            // ?.querySelector(".update-components-text")?.innerHTML,
             totalLikes: +(
               parent
                 ?.querySelector(
                   ".social-details-social-counts__reactions-count, .social-details-social-counts__social-proof-text"
                 )
-                ?.textContent?.match(/\d/g)?.join('') || 0
+                ?.textContent?.match(/\d/g)
+                ?.join("") || 0
             ),
-          };
+          }
         })
         .filter((f) => f.id)
-        .slice(0, 4);
-    });
+        .slice(0, 4)
+    })
 
     if (!ids.length) {
-      return;
+      return
     }
 
     for (const id of ids) {
@@ -93,39 +105,61 @@ export class LinkedinEngagementService
           undefined,
           false,
           -700
-        );
-        await timer(1000);
-        await this.moveAndClick(page, `#${id.like}`);
-        await timer(1000);
+        )
+        await timer(1000)
+        await this.moveAndClick(page, `#${id.like}`)
+        await timer(1000)
+        if (data?.AIGeneratedComments?.enabled) {
+          if (id.totalLikes > 15 && id.text?.length > 150) {
+            const input = ""
+            const post = id.text // Post
+            const person = "technical person" // Briefly describe yourself (e.g. 'sales guy')
+            console.log("Applying texterous.com's magic!")
+            const response = await axios.post(
+              "https://us-central1-alpaca-383f8.cloudfunctions.net/api/api/completion?api-key=" +
+                data?.AIGeneratedComments?.texterousApiToken,
+              {
+                data: {
+                  input,
+                  applet: "XTVLt0UAsmDVLAxF5Of0",
+                  inputs: { post, person },
+                },
+              },
+              { timeout: 10000 }
+            )
 
-        if (id.totalLikes > 30) {
+            console.log(id.text, response.data.completion?.split("\n")?.[0])
+          }
+        } else if (id.totalLikes > 30) {
           await this.moveMouseAndScroll(
             page,
             `#${id.comment}`,
             undefined,
             false,
             -700
-          );
-          await timer(1000);
-          await this.moveAndClick(page, `#${id.comment}`);
-          await timer(1000);
-          await page.keyboard.type(
-            shuffle([
-              "Thank you for sharing",
-              "Great Share",
-              "Cool",
-              "Thanks for posting 💯🔥",
-              "💯💯",
-              "Great content keep it up 👌🏼",
-              "Great 👍",
-              "Awesome!!",
-            ])[0],
-            { delay: 20 }
-          );
-          await timer(1000);
-          await this.moveAndClick(page, ".comments-comment-box__submit-button");
+          )
+          await timer(1000)
+          await this.moveAndClick(page, `#${id.comment}`)
+          await timer(1000)
+          // await page.keyboard.type(
+          //   shuffle([
+          //     "Thank you for sharing",
+          //     "Great Share",
+          //     "Cool",
+          //     "Thanks for posting 💯🔥",
+          //     "💯💯",
+          //     "Great content keep it up 👌🏼",
+          //     "Great 👍",
+          //     "Awesome!!",
+          //   ])[0],
+          //   { delay: 20 }
+          // )
+          // await timer(1000)
+          // await this.moveAndClick(page, ".comments-comment-box__submit-button")
         }
-      } catch (err) {}
+      } catch (err) {
+        console.log(JSON.stringify(err))
+      }
     }
   }
 }
--- lib/test.example.ts
+++ lib/test.example.ts
@@ -1,42 +1,57 @@
-import * as LinvoScraper from './linkedin.service';
-import * as puppeteer from 'puppeteer';
+import * as LinvoScraper from "./linkedin.service"
+import puppeteer from "puppeteer-extra"
 
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-    const page = (await browser.newPage());
-    const cdp = await page.target().createCDPSession();
+// add stealth plugin and use defaults (all evasion techniques)
+import StealthPlugin from "puppeteer-extra-plugin-stealth"
+puppeteer.use(StealthPlugin())
 
-    // that's the res Linvo is working in production
-    await page.setViewport({
-        width: 1440,
-        height: 900,
-    });
+import envy from "envy"
 
+const env = envy()
+console.log(env)
+;(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+  })
+  const page = await browser.newPage()
+  const cdp = await page.target().createCDPSession()
+
+  // that's the res Linvo is working in production
+  await page.setViewport({
+    width: 1440,
+    height: 900,
+  })
+
+  try {
     // add ghost-cursor for maximum safety
-    await LinvoScraper.tools.loadCursor(page, true);
-
+    await LinvoScraper.tools.loadCursor(page, true)
     // Login with Linkedin
-    const {token} = await LinvoScraper.services.login.process(page, cdp, {
-        user: 'bang@linvo.io',
-        password: 'superStrongPass!!%'
+    console.log(env)
+    const { token } = await LinvoScraper.services.login.process(page, cdp, {
+      user: env.linkedinUser,
+      password: env.linkedinPassword,
     })
 
     // set cookies
     await page.setCookie({
-        name: "li_at",
-        value: token,
-        httpOnly: true,
-        secure: true,
-        sameSite: "None",
-        priority: "Medium",
-        path: "/",
-        domain: ".www.linkedin.com",
-    });
-
-    await LinvoScraper.services.connect.process(page, cdp, {
-        message: 'Hi Nevo! Let\'s connect!',
-        url: 'https://www.linkedin.com/in/nevo-david/'
+      name: "li_at",
+      value: token,
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+      priority: "Medium",
+      path: "/",
+      domain: ".www.linkedin.com",
     })
-})();
+
+    await LinvoScraper.services.engagement.process(page, cdp, {
+      AIGeneratedComments: {
+        enabled: true,
+        texterousApiToken: env.texterousApiKey,
+      },
+    })
+  } catch (error) {
+    console.log(JSON.stringify(error))
+  }
+  ;``
+})()
